5d5a4af7b2e90677f5c3573191317df56ba2555d
Espe Cabrer <mecabrerv@gmail.com>
2022-10-07 14:43:06 +0200

    first commit

---
+++ src/main/java/com/ironhack/project_crm_2/ProjectCrm2Application.java
@@ -0,0 +1,13 @@
+package com.ironhack.project_crm_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCrm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCrm2Application.class, args);
+	}
+
+}
