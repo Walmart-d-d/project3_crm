f30059bd737ea27efac8978c4ae15c5c8d13189c
Espe Cabrer <mecabrerv@gmail.com>
2022-10-09 00:07:30 +0200

    Feat: lead menu and tests

--- src/main/java/com/ironhack/project_crm_2/classes/Menu.java
+++ src/main/java/com/ironhack/project_crm_2/classes/Menu.java
@@ -2,6 +2,7 @@ package com.ironhack.project_crm_2.classes;
 
 import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Menu {
@@ -23,11 +24,12 @@ public class Menu {
     }
 
     public static String getInputMainMenu() {
-        List<String> validOptions = List.of("1", "2", "3");
+        List<String> validOptions = List.of("1", "2", "3", "4");
 
-        System.out.println("1. Leads and opportunities");
-        System.out.println("2. Accounts");
-        System.out.println("3. Exit");
+        System.out.println("1. Leads");
+        System.out.println("2. Opportunities");
+        System.out.println("3. Accounts");
+        System.out.println("4. Exit");
         String option = input.nextLine();
 
         if(!validOptions.contains(option)) {
@@ -39,16 +41,68 @@ public class Menu {
     public static void manageOptionMainMenu(String option){
         switch (option) {
             case "1":
-                //leadMenu();
-                System.out.println("leadMenu");
+                leadMenu();
                 break;
             case "2":
-                //accountMenu();
-                System.out.println("accountMenu");
+                //accountMenu(); --> opportunities
+                System.out.println("oppMenu");
                 break;
             case "3":
+                //accounts
+                break;
+            case "4":
                 System.exit(0);
                 break;
         }
+    }
+
+    public static void leadMenu(){
+        boolean selectValidOption = false;
+
+        while(!selectValidOption){
+            try {
+                String mainMenuOption = Menu.getInputLeadMenu();
+                Menu.manageOptionLeadMenu(mainMenuOption);
+                selectValidOption = true;
+            } catch (InputMismatchException e) {
+                System.err.println("Select a valid option");
+            }
+        }
+    }
+
+    public static String getInputLeadMenu(){
+        List<String> validOptions = List.of("1", "2", "3", "4", "5");
+
+        System.out.println("1. Create Lead");
+        System.out.println("2. Show all Leads");
+        System.out.println("3. Show Lead by Identification Number");
+        System.out.println("4. Convert Lead into Opportunity");
+        System.out.println("5. Go back");
+        String option = input.nextLine();
+
+        if(!validOptions.contains(option)) {
+            throw new InputMismatchException("Invalid option");
+        }
+        return option;
+    }
+
+    public static void manageOptionLeadMenu(String option){
+            switch (option) {
+                case "1":
+                    System.out.println("create lead");
+                    break;
+                case "2":
+                    System.out.println("show leads");
+                    break;
+                case "3":
+                    System.out.println("Show lead by id");
+                    break;
+                case "4":
+                    System.out.println("Convert lead into opp");
+                    break;
+                case "5":
+                    System.out.println("go back");
+                    break;
+            }
     }
 }
--- src/test/java/com/ironhack/project_crm_2/classes/MenuTests.java
+++ src/test/java/com/ironhack/project_crm_2/classes/MenuTests.java
@@ -13,7 +13,7 @@ public class MenuTests {
 
     @Test
     @DisplayName("Main menu choose valid option - works ok")
-    void mainMenuChooseOption_ValidInput_WorksOk(){
+    void getInputMainMenu_ValidInput_WorksOk(){
         String userInput = "1";
         ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
         System.setIn(bais);
@@ -22,11 +22,29 @@ public class MenuTests {
 
     @Test
     @DisplayName("Main menu choose invalid option - throw error")
-    void mainMenuChooseOption_InvalidInput_ThrowError(){
+    void getInputMainMenu_InvalidInput_ThrowError(){
         String userInput = "6";
         ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
         System.setIn(bais);
         assertThrows(InputMismatchException.class, Menu::getInputMainMenu);
+    }
+
+    @Test
+    @DisplayName("Lead menu choose valid option - works ok")
+    void getInputLeadMenu_ValidInput_WorksOk(){
+        String userInput = "1";
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(bais);
+        assertEquals("1", Menu.getInputLeadMenu());
+    }
+
+    @Test
+    @DisplayName("Lead menu choose invalid option - throw error")
+    void getInputLeadMenu_InvalidInput_ThrowError(){
+        String userInput = "6";
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(bais);
+        assertThrows(InputMismatchException.class, Menu::getInputLeadMenu);
     }
 
 }
